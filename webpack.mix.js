fa3f2207e23a89af9d74685b6c78ef533eb62e2a
LorensCuscan <lorenscuscan@gmail.com>
2023-08-07 23:04:30 -0300

    all assets compiled

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,12 +12,36 @@ const mix = require('laravel-mix');
  */
 
 mix
-.styles([
-    'resources/views/front/assets/css/animate.css',
-    'resources/views/front/assets/css/icomoon.css',
-    'resources/views/front/assets/css/bootstrap.css',
-    'resources/views/front/assets/css/magnific-popup.css',
-    'resources/views/front/assets/css/owl.carousel.min.css',
-    'resources/views/front/assets/css/owl.theme.default.min.css',
-], 'public/front/assets/css/vendor.css')
+    .styles([
+        'resources/views/front/assets/css/animate.css',
+        'resources/views/front/assets/css/icomoon.css',
+        'resources/views/front/assets/css/bootstrap.css',
+        'resources/views/front/assets/css/magnific-popup.css',
+        'resources/views/front/assets/css/owl.carousel.min.css',
+        'resources/views/front/assets/css/owl.theme.default.min.css',
+    ], 'public/front/assets/css/vendor.css')
+
+    .scripts([
+        'resources/views/front/assets/js/modernizr-2.6.2.min.js'
+    ], 'resources/views/front/assets/js/modernizr.js')
+
+    .scripts([
+        'resources/views/front/assets/js/respond.min.js'
+    ], 'public/views/front/assets/js/respond.min.js')
+
+    .scripts([
+        'resources/views/front/assets/js/jquery.min.js',
+        'resources/views/front/assets/js/jquery.easing.1.3.js',
+        'resources/views/front/assets/js/bootstrap.min.js',
+        'resources/views/front/assets/js/jquery.waypoints.min.js',
+        'resources/views/front/assets/js/jquery.stellar.min.js',
+        'resources/views/front/assets/js/owl.carousel.min.js',
+        'resources/views/front/assets/js/jquery.countTo.js',
+        'resources/views/front/assets/js/magnific-popup-options.js',
+        'resources/views/front/assets/js/main.js'
+    ], 'public/views/front/assets/js/vendor.js')
+
+    .scripts([
+        'resources/views/front/assets/js/main.js'
+    ], 'public/views/front/assets/js/main.js')
 
